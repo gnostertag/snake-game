f10f609e2c1d1d0a46ee9c14fc353a567a34ded6
gnostertag <ostertag2000@gmail.com>
2023-07-11 15:38:29 -0500

    added grid

--- src/GamePanel.java
+++ src/GamePanel.java
@@ -22,15 +22,30 @@ public class GamePanel extends JPanel implements ActionListener{
 	Random random;
 			
 	GamePanel(){
-		
+		random = new Random();
+		this.setPreferredSize(new Dimension(SCREEN_WIDTH,SCREEN_HEIGHT));
+		this.setBackground(Color.black);
+		this.setFocusable(true);
+		this.addKeyListener(new MyKeyAdapter());
+		startGame();
 	}
 	public void startGame() {
-		
+		newApple();
+		running = true;
+		timer = new Timer(DELAY,this);
+		timer.start();
 	}
 	public void paintComponent(Graphics g) {
-		
+		super.paintComponent(g);
+		draw(g);
 	}
 	public void draw(Graphics g) {
+		for(int i=0;i<SCREEN_HEIGHT/UNIT_SIZE;i++) {
+			g.drawLine(i*UNIT_SIZE, 0, i*UNIT_SIZE, SCREEN_HEIGHT);
+			g.drawLine(0, i*UNIT_SIZE, SCREEN_WIDTH, i*UNIT_SIZE);
+		}
+	}
+	public void newApple() {
 		
 	}
 	public void move() {
