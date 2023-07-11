70e617b2a2518f213b01ab7e2deca80c93e2cd75
gnostertag <ostertag2000@gmail.com>
2023-07-11 14:58:13 -0500

    new class

--- src/GamePanel.java
+++ src/GamePanel.java
@@ -1,5 +1,8 @@
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+import java.awt.*;
+//import java.awt.event.ActionEvent;
+//import java.awt.event.ActionListener;
+//import java.awt.event.KeyAdapter;
+import java.awt.event.*;
 
 import javax.swing.JPanel;
 
@@ -11,12 +14,35 @@ public class GamePanel extends JPanel implements ActionListener{
 	public void startGame() {
 		
 	}
-	public void paintComponent(Graphics g)
-4.57
+	public void paintComponent(Graphics g) {
+		
+	}
+	public void draw(Graphics g) {
+		
+	}
+	public void move() {
+		
+	}
+	public void checkApple() {
+		
+	}
+	public void checkCollisions() {
+		
+	}
+	public void gameOver(Graphics g) {
+		
+	}
+
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		// TODO Auto-generated method stub
 		
+	}
+	public class MyKeyAdapter extends KeyAdapter{
+		@Override
+		public void keyPressed(KeyEvent e) {
+			
+		}
 	}
  
 }
