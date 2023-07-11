1731e25cb68f34f971809d3b0aab76597641cb1c
gnostertag <ostertag2000@gmail.com>
2023-07-11 15:10:05 -0500

    declarations

--- src/GamePanel.java
+++ src/GamePanel.java
@@ -1,13 +1,26 @@
 import java.awt.*;
-//import java.awt.event.ActionEvent;
-//import java.awt.event.ActionListener;
-//import java.awt.event.KeyAdapter;
 import java.awt.event.*;
-
-import javax.swing.JPanel;
+import javax.swing.*;
+import java.util.Random;
 
 public class GamePanel extends JPanel implements ActionListener{
 	
+	static final int SCREEN_WIDTH = 600;
+	static final int SCREEN_HEIGHT = 600;
+	static final int UNIT_SIZE = 25;
+	static final int GAME_UNITS = (SCREEN_WIDTH*SCREEN_HEIGHT)/UNIT_SIZE;
+	static final int DELAY = 75;
+	final int x[] = new int[GAME_UNITS];	
+	final int y[] = new int[GAME_UNITS];
+	int bodyParts = 5;
+	int applesEaten;
+	int appleX;
+	int appleY;
+	char direction = 'R';
+	boolean running = false;
+	Timer timer;
+	Random random;
+			
 	GamePanel(){
 		
 	}
